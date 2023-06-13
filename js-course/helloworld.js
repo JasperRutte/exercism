087b361066e5e5bf162c324788359fb2f2a899fd
Jasper Rutte <jasperrutte@gmail.com>
2023-06-14 00:14:10 +0200

    finished Annalyn's Infiltration

--- js-course/helloworld.js
+++ js-course/helloworld.js
@@ -1,4 +1,4 @@
-git //
+//
 // This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
---
+++ js-course/nnalynsInfiltration.js
@@ -0,0 +1,22 @@
+export function canExecuteFastAttack(knightIsAwake) {
+    return !knightIsAwake;
+}
+
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+    return (knightIsAwake || archerIsAwake || prisonerIsAwake);
+}
+
+export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+    return !archerIsAwake && prisonerIsAwake;
+}
+
+export function canFreePrisoner(
+    knightIsAwake,
+    archerIsAwake,
+    prisonerIsAwake,
+    petDogIsPresent
+) {
+    if (petDogIsPresent && !archerIsAwake){
+        return true
+    } else return prisonerIsAwake && !archerIsAwake && !knightIsAwake;
+}
